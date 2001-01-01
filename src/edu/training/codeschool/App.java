71c33a7c7a6e134372104e363ba3963c4bcfb255
Hrach <hrach9012@gmail.com>
2001-01-01 04:05:07 +0400

    first test

--- src/edu/training/codeschool/App.java
+++ src/edu/training/codeschool/App.java
@@ -1,7 +1,18 @@
 package edu.training.codeschool;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+
+
+        }
+
+
     }
-}
+
+
+
+
+
+
+
+
