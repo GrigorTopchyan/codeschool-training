6df492c27b8524389e0c837a859cfd26042459a4
Hrach <hrach9012@gmail.com>
2020-01-19 16:44:27 +0400

    first commit

---
+++ src/edu/ExapmleWithOperators.java
@@ -0,0 +1,13 @@
+package edu;
+
+public class ExapmleWithOperators {
+
+    public static void main(String[] args) {
+        int num;
+        num = 28;
+        System.out.println(num);
+        int result = num * num +5;
+        System.out.println("The valuof result is");
+        System.out.println(result);
+    }
+}
