1e3506670f657dc6a04ed2a21fbbbfe00bd58e30
ArsenYeghikyan <arsegikyan@gmail.com>
2020-01-19 03:45:40 -0800

    First commit

---
+++ src/edu/training/codeschool/ExampleWithOperators.java
@@ -0,0 +1,12 @@
+package edu.training.codeschool;
+
+public class ExampleWithOperators {
+    public static void main(String[] args) {
+        int num;
+        num = 20;
+        System.out.println("The value of num is "+num);
+        int result = num*num+5;
+        System.out.print("The value of result is ");
+        System.out.println(result);
+    }
+}
