cd50736e9ea044db1cdbc557d5930549cfee9fd5
kgevorg98 <kgevorg1998@gmail.com>
2020-01-19 16:44:45 +0400

    first commit

---
+++ src/edu/training/codeschool/ExampleWithOperators.java
@@ -0,0 +1,11 @@
+package edu.training.codeschool;
+
+public class ExampleWithOperators {
+    public static void main(String[] args) {
+        int num;
+        num = 20;
+        System.out.println("the value of num "+num);
+        int result = num*num+5;
+        System.out.println("the new value is "+result);
+    }
+}
