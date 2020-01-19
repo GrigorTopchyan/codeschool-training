d312a8f3cb83365a0f873045244bee5ca009e67a
SAROJIANI <60001760+SAROJIANI@users.noreply.github.com>
2020-01-19 04:42:13 -0800

    First commit

--- src/edu/training/codeschool/App.java
+++ src/edu/training/codeschool/App.java
@@ -2,6 +2,7 @@ package edu.training.codeschool;
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/edu/training/codeschool/ExamplewithOperators.java
@@ -0,0 +1,12 @@
+package edu.training.codeschool;
+
+public class ExamplewithOperators {
+    public static void main (String [] args){
+        int num;
+        num=20;
+        System.out.println ("The value of " + num);
+        int result=num*num+5;
+        System.out.println("The value of result is");
+        System.out.println(result);
+    }
+}
