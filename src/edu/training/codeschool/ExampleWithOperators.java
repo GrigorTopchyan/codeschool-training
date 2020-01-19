289726c3b98105c54b7982411f47605b2727cf9e
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-19 18:58:51 +0400

    CoodSchool

---
+++ src/edu/training/codeschool/ExampleWithOperators.java
@@ -0,0 +1,13 @@
+package edu.training.codeschool;
+
+import java.sql.SQLOutput;
+
+public class ExampleWithOperators {
+    public static void main(String[] args) {
+        int num;
+        num = 20;
+        System.out.println("The value of num is " + num);
+        int result = num * num + 5;
+        System.out.println("the new value is " + result);
+    }
+}
