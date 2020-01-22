e90b0a561d6a3d33c20639a5b4eb12e770fe67e9
Vanik <vanik.man5@gmail.com>
2020-01-22 23:00:29 +0400

    H

---
+++ src/edu/training/codeschool/A.java
@@ -0,0 +1,5 @@
+package edu.training.codeschool;
+
+public class A {
+
+}
--- src/edu/training/codeschool/App.java
+++ src/edu/training/codeschool/App.java
@@ -3,5 +3,8 @@ package edu.training.codeschool;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+
     }
 }
