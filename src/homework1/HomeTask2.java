d94457f1727d72d07c99eacc4c8770eaf8b52bf8
h-kocharyan <hkocharyan94@gmail.com>
2020-01-23 21:36:49 +0400

    task 1

---
+++ src/homework1/HomeTask2.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 73;
+        int S = a * b;
+        System.out.println("The area of rectangle is " + S +"m");
+
+    }
+}
