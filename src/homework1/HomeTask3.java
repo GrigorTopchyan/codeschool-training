5b55d303f97bd1d14ae3eb4de7791bbc5c7259b6
h-kocharyan <hkocharyan94@gmail.com>
2020-01-23 22:05:37 +0400

    task 3

---
+++ src/homework1/HomeTask3.java
@@ -0,0 +1,17 @@
+package homework1;
+
+import com.sun.deploy.security.SelectableSecurityManager;
+
+public class HomeTask3 {
+    public static void main(String[] args) {
+        int a = 24654;
+        int b = 2;
+        int c = a % b;
+        if (c == 0)
+            System.out.println("The number is odd");
+        else
+            System.out.println("The number is even");
+
+
+    }
+}
