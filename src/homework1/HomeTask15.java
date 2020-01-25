b901b07afddfee06367d9d750be227ddadc6fd96
h-kocharyan <hkocharyan94@gmail.com>
2020-01-25 22:40:54 +0400

    task 15

---
+++ src/homework1/HomeTask15.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class HomeTask15 {
+    public static void main(String[] args) {
+        float number1 = 4;
+        float number2 = 7;
+        float number3 = 24;
+        float number4 = 76;
+        float number5 = 832;
+        float sum = number1 + number2 + number3 + number4 + number5;
+        System.out.println("The sum of given 5 numbers is "+ sum);
+        float min= sum / 5;
+        System.out.println("The average of given 5 numbers is "+ min);
+
+    }
+}
