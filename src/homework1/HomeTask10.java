65550d16d2f64be230a9242070f53de5ba6f6ba0
h-kocharyan <hkocharyan94@gmail.com>
2020-01-25 23:40:44 +0400

    task 10

---
+++ src/homework1/HomeTask10.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class HomeTask10 {
+    public static void main(String[] args) {
+//        10.  Write a Java program to print the sum (addition),
+//        multiply of 4 numbers(number1, number2, number3, number4).
+        int number1 = 45;
+        int number2 = 23;
+        int number3 = 71;
+        int number4 = 49;
+        int sum = number1+ number2 +number3 + number4;
+        int multiply = number1* number2 *number3 * number4;
+        System.out.println("The sum is "+ sum);
+        System.out.println("The multiply is "+ multiply);
+    }
+}
