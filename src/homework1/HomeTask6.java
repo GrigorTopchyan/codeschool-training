06ee4a328e3a43cc137214fe39ca023e5b31d8d3
Gevorg Mkhitaryan <gevorg.mkhitaryan.02@gmail.com>
2020-01-25 14:29:15 +0400

    task6  task7

---
+++ src/homework1/HomeTask6.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class HomeTask6 {
+    public static void main(String[] args) {
+
+        int a = 147;
+        boolean b = a % 3 == 0;
+        boolean c = a % 7 == 0;
+        boolean d = b && c;
+        System.out.println(d);
+
+    }
+}
+
---
+++ src/homework1/HomeTask7.java
@@ -0,0 +1,13 @@
+package homework1;
+
+public class HomeTask7 {
+    public static void main(String[] args){
+
+        int a = 150;
+        boolean b = a % 5 == 0;
+        boolean c = a % 6 == 0;
+        boolean d = b || c;
+        System.out.println(d);
+
+    }
+}
