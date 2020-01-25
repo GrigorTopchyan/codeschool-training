9ad98f560831edae543b56c3d5981c51c30c6739
h-kocharyan <hkocharyan94@gmail.com>
2020-01-26 00:21:26 +0400

    task 14

---
+++ src/homework1/HomeTask14.java
@@ -0,0 +1,12 @@
+package homework1;
+
+public class HomeTask14 {
+    public static void main(String[] args) {
+        String str = "v";
+        String result = str.toUpperCase();
+        if (str == str.toUpperCase())
+            System.out.println(str.toLowerCase());
+        else if (str == str.toLowerCase())
+            System.out.println(str.toUpperCase());
+    }
+}
