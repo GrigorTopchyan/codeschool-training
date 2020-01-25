ad589b610877881f8fb19fbae49e9dfd002caaa1
h-kocharyan <hkocharyan94@gmail.com>
2020-01-25 22:34:14 +0400

    task 16

---
+++ src/homework1/HomeTask16.java
@@ -0,0 +1,11 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class HomeTask16 {
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter your weight ");
+        System.out.println(" Your weight on Moon is "+ scanner.nextDouble() * 0.17);
+    }
+}
