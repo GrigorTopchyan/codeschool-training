785c250d46cf33376158acc58b3c512e7f22750b
SurenParsadanyan <Komitas1707>
2020-01-25 19:20:22 +0400

    HomeTask2

---
+++ src/homework1/HomeTask2.java
@@ -0,0 +1,16 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        System.out.println("Mutqagreq uxxankyan erkarutyun@");
+        Scanner scanner = new Scanner (System.in);
+        int x = scanner.nextInt();
+        System.out.println("Mutqagreq uxxankyan laynutyun@");
+        Scanner scanner1 = new Scanner (System.in);
+        int y = scanner1.nextInt();
+        int square = x * y;
+        System.out.println("Uxxankyan makeres@ = " + square);
+    }
+}
