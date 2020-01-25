9288c9d59ae14a7065a54310423cf6336add41f0
Gevorg Mkhitaryan <gevorg.mkhitaryan.02@gmail.com>
2020-01-25 17:25:34 +0400

    task13

--- src/homework1/HomeTask13.java
+++ src/homework1/HomeTask13.java
@@ -1,22 +1,51 @@
 package homework1;
 
+import java.util.Scanner;
 
 public class HomeTask13 {
     public static void main(String[] args) {
 
-        int x = 0;
-        System.out.print("number");
-        if (x == 0) {
-            System.out.println(" zero");
-        } else if (x > 0) {
-            System.out.println(" positive");
-        } else if (x < 0) {
-            System.out.println(" negative");
-        }else if (x<1){
-            System.out.println(" small");
-        }else if(x>1000000){
-            System.out.println(" large");
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.print("number: ");
+            double input = in.nextDouble();
+
+            if (input > 0)
+            {
+                if (input < 1)
+                {
+                    System.out.println(" small");
+                }
+                else if (input > 1000000)
+                {
+                    System.out.println(" large");
+                }
+                else
+                {
+                    System.out.println(" Positive");
+                }
+            }
+            else if (input < 0)
+            {
+                if (Math.abs(input) < 1)
+                {
+                    System.out.println("Negative small");
+                }
+                else if (Math.abs(input) > 1000000)
+                {
+                    System.out.println(" Negative large");
+                }
+                else
+                {
+                    System.out.println(" Negative");
+                }
+            }
+            else
+            {
+                System.out.println(" Zero");
+            }
         }
+
 
     }
 }
