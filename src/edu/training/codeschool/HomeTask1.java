4009761d537afbbfc4766827dc0a45f119a42328
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-25 11:33:53 +0400

    homtask 1 skzbi popoxakannery chei grel stex avelacrelem

--- src/edu/training/codeschool/HomeTask1.java
+++ src/edu/training/codeschool/HomeTask1.java
@@ -1,29 +1,26 @@
 package edu.training.codeschool;
 
-import com.sun.org.apache.xpath.internal.objects.XString;
-import org.omg.CosNaming.IstringHelper;
-
 public class HomeTask1 {
     public static void main(String[] args) {
         int a = 10;
         int b = 14;
         int c = 15;
         int d = 20;
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a / b);
-        System.out.println(a * b);
-        System.out.println(b % a);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b % a = " + (b % a));
         a += b;
-        System.out.println(a);
+        System.out.println("a = " + a);
         a -= b;
-        System.out.println(a);
+        System.out.println("a = " + a);
         c *= d;
-        System.out.println(c);
+        System.out.println("c = " + c);
         c /= d;
-        System.out.println(c);
+        System.out.println("c = " + c);
         d %= c;
-        System.out.println(d);
+        System.out.println("d = " + d);
 
 
     }
