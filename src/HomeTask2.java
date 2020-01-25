f652242ab86b92c6d4a3c8b3d8cc8677058adca5
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-25 11:42:08 +0400

    homtask 2

---
+++ src/HomeTask2.java
@@ -0,0 +1,7 @@
+public class HomeTask2 {
+    public static void main(String[] args) {
+        int height = 5;
+        int width = 10;
+        System.out.println("area of rectangle = " +( width * height ));
+    }
+}
