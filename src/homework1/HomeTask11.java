9903d27f4597080ed31aed96bfa831d66d51b5dd
h-kocharyan <hkocharyan94@gmail.com>
2020-01-25 23:34:09 +0400

    task 11, bayc karces te sxal em tiv@ stanum

---
+++ src/homework1/HomeTask11.java
@@ -0,0 +1,17 @@
+package homework1;
+
+public class HomeTask11 {
+    public static void main(String[] args) {
+//        Write a Java program to compute a specified formula
+//        4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+        double a = 4.0;
+        double b = 1;
+        double c = 1.0;
+        double d ;
+        for (d = 1.0/3; d <= 11; d = d + 2) ;
+        System.out.println( a *(b -(c /d)));
+
+
+
+    }
+}
