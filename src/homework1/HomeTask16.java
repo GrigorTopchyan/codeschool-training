46e5cfc9fb448b901caee1dba25b3d132f31ea4c
Gevorg Mkhitaryan <gevorg.mkhitaryan.02@gmail.com>
2020-01-25 17:32:06 +0400

    task16

--- src/homework1/HomeTask16.java
+++ src/homework1/HomeTask16.java
@@ -2,13 +2,11 @@ package homework1;
 
 public class HomeTask16 {
     public static void main(String[] args) {
-        double g = 9.8;
-        int m = 70;
-        double f = g * m;
-        double f1 = 17.0;
-        double k = (f * f1) / 100;
-        double x = (f - k) / g;
-        System.out.println("weight on the moon = " + x);
+        int a = 70;
+        double f = 17.0;
+        double k = (a * f) / 100;
+        double c = a - k;
+        System.out.println("weight on the moon = " + c);
 
 
     }
