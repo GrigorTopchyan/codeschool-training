ccfa250b97f06d8545c8341c7f46119f9a5bdd0b
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-26 00:28:17 +0400

    homtask 6

---
+++ src/HomeTask6.java
@@ -0,0 +1,10 @@
+public class HomeTask6 {
+    public static void main(String[] args) {
+
+        int a=21;
+        if (a%3==0&&a%7==0)
+        System.out.println("The digit divides onto 3 and 7");
+        else System.out.println("The digit doesn't divide onto 3 and 7");
+
+    }
+}
