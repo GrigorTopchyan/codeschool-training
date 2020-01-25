e5d1ac4e3ddebf4b968edb977eb50b5e06afa21c
SurenParsadanyan <Komitas1707>
2020-01-25 19:28:11 +0400

    HomeTask3

---
+++ src/homework1/HomeTask3.java
@@ -0,0 +1,16 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class HomeTask3 {
+    public static void main(String[] args) {
+        System.out.println("Mutqagreq cankacac amboxj tiv:");
+        Scanner scanner = new Scanner (System.in);
+        int x = scanner.nextInt();
+        if (x % 2 == 0){
+            System.out.println("Mutqagrac tiv@ zuyg e");
+        }else{
+            System.out.println("Mutqagrac tiv@ kent e");
+        }
+    }
+}
