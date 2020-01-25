fa38a19812ed2e5c9c31fc74b4f4e66ecef7eaf3
SurenParsadanyan <Komitas1707>
2020-01-25 18:34:50 +0400

    HomeTask9

---
+++ src/homework1/HomeTask9.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class HomeTask9 {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 * 6);
+
+        System.out.println((55 + 9) % 9);
+
+        int x = -3 * 5;
+        float y = (float)x/8;
+        System.out.println(20 + y);
+
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
