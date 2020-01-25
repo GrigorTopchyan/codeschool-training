752a252e5f1957c6e262746d9f1de29ff50d756e
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-26 00:35:04 +0400

    homtask 7

---
+++ src/Hometask7.java
@@ -0,0 +1,10 @@
+public class Hometask7 {
+    public static void main (String[] args) {
+
+
+        int a=30;
+        if (a%5==0||a%6==0)
+            System.out.println("The digit divides onto 5 or 6");
+        else System.out.println(" The digit doesn't divide onto 5 or 6");
+    }
+}
