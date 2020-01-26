ea82bfe827e5d24633b56c98098946dfab8a9ac4
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-26 10:56:17 +0400

    homtask 10

---
+++ src/Homtask10.java
@@ -0,0 +1,10 @@
+public class Homtask10 {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 12;
+        int number3 = 15;
+        int number4 = 20;
+        System.out.println("number1 + number2 + number3 + number4 = " +(number1 + number2 + number3 + number4) );
+        System.out.println("number1 * number2 * number3 * number4 = " + number1 * number2 * number3 * number4 );
+    }
+}
