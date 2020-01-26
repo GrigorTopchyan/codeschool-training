e74c90c73bd77c7217fcc270a8d2e48f2f273adb
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-26 11:10:00 +0400

    homtask 14

---
+++ src/HomeTask14.java
@@ -0,0 +1,11 @@
+public class HomeTask14 {
+    public static void main(String[] args) {
+        int number1 = 3;
+        int number2 = 4;
+        int number3 = 6;
+        int number4 = 7;
+        int number5 = 10;
+        System.out.println("Sum = " + (number1 + number2 + number3 + number4 + number5));
+        System.out.println("Average = " + (number1 + number2 + number3 + number4 + number5)/5);
+    }
+}
