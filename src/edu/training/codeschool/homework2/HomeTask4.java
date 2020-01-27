efecb1ce1b39e09e354d51081671df915d8454c2
h-kocharyan <hkocharyan94@gmail.com>
2020-01-27 19:56:51 +0400

    task 4

---
+++ src/edu/training/codeschool/homework2/HomeTask4.java
@@ -0,0 +1,26 @@
+package edu.training.codeschool.homework2;
+
+import com.sun.org.apache.xpath.internal.objects.XBoolean;
+
+import java.util.Scanner;
+
+//     4. Given n (Input n from console). Write a program
+//     that prints "The number is prime" if the given number is prime,
+//             otherwise it prints "The number is not prime".
+//             (Note that the number is prime if it divides on 1 and on itself
+public class HomeTask4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        boolean isPrime = true;
+        for (int j = 2; j <= a / 2; j++) {
+            if (a % j == 0) {
+                isPrime = false;
+                System.out.println("The number isn`t prime");
+                break;
+            }
+
+        }
+        System.out.println("The number is prime");
+    }
+}
