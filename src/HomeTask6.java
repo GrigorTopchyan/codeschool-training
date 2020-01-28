45cc1ab2a2694c4ca723a600d26f59246ef774a2
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-28 20:15:06 +0400

    es uxxelem tivy konsulica uzum ...u avelacrelem if-nested-if ov

--- src/HomeTask6.java
+++ src/HomeTask6.java
@@ -15,7 +15,7 @@ public class HomeTask6 {
             System.out.println("The digit divides onto 3 and 7(for if-nested-if)");
         }
         if (x % 3 != 0 && x % 7 != 0) {
-            System.out.println("The digit divides onto 3 and 7(for if-nested-if)");
+            System.out.println("The digit doesn't divide onto 3 and 7(for if-nested-if)");
         }
 
 
--- src/Hometask7.java
+++ src/Hometask7.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Hometask7 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
 
-        int a=30;
-        if (a%5==0||a%6==0)
-            System.out.println("The digit divides onto 5 or 6");
-        else System.out.println(" The digit doesn't divide onto 5 or 6");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println();
+        // if-else-if
+        if (x % 5 == 0 || x % 6 == 0) {
+            System.out.println("The digit divides onto 5 and 6(for if-else-if)");
+        } else System.out.println("The digit doesn't divide onto 5 and 6(for if-else-if)");
+        //if-nested-if
+        if (x % 5 == 0 || x % 6 == 0) {
+            System.out.println("The digit divides onto 5 and 6(for if-nested-if)");
+        }
+        if (x % 5 != 0 || x % 6 != 0) {
+            System.out.println("The digit doesn't divide onto 5 or 6(for if-nested-if)");
+        }
     }
 }
