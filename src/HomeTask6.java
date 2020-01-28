0a36792d2b79a9c4efe27030661af2743225cfce
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-28 20:05:48 +0400

    es uxxelem tivy konsulica uzum ...u avelacrelem if-nested-if ov

--- src/HomeTask6.java
+++ src/HomeTask6.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class HomeTask6 {
     public static void main(String[] args) {
 
-        int a=21;
-        if (a%3==0&&a%7==0)
-        System.out.println("The digit divides onto 3 and 7");
-        else System.out.println("The digit doesn't divide onto 3 and 7");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println();
+        // if-else-if
+        if (x % 3 == 0 && x % 7 == 0) {
+            System.out.println("The digit divides onto 3 and 7(for if-else-if)");
+        } else System.out.println("The digit doesn't divide onto 3 and 7(for if-else-if)");
+        //if-nested-if
+        if (x % 3 == 0 && x % 7 == 0) {
+            System.out.println("The digit divides onto 3 and 7(for if-nested-if)");
+        }
+        if (x % 3 != 0 && x % 7 != 0) {
+            System.out.println("The digit divides onto 3 and 7(for if-nested-if)");
+        }
+
 
     }
 }
