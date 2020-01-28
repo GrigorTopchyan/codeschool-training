871df5c51db53aeae40577e823509b1f87731b72
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-28 23:41:08 +0400

    Grigor jan es erkarei grel qo asac dzevov em sarqel

--- src/HomeTask8.java
+++ src/HomeTask8.java
@@ -1,24 +1,32 @@
+import com.sun.xml.internal.fastinfoset.tools.XML_SAX_StAX_FI;
+
 ;
 
 public class HomeTask8 {
     public static void main(String[] args) {
         double n1 = 1.2, n2 = 0.6, n3 = -2.5, n4 = 0;
-        if (n1 > n2 && n1 > n3 && n1 > n4)
-        System.out.println(" max = 1.2");
-        if (n1 < n2&&n1 < n3&&n1 < n4)
-        System.out.println("min = 1.2");
-        if (n2 > n1 && n2 > n3 && n2 > n4)
-        System.out.println("max = 0.6");
-        if (n2 < n1 && n2 < n3 && n2 < n4)
-        System.out.println("min = 0.6");
-        if (n3 > n1 && n3 > n2 && n3 > n4)
-            System.out.println("max = -2.5");
-        if (n3 < n1 && n3 < n2 && n3 < n4)
-            System.out.println("min = -2.5");
-        if (n4 > n1 && n4 > n2 && n4 > n3)
-            System.out.println("max = 0");
-        if (n4 < n1 && n4 < n2 && n4 < n3)
-            System.out.println("min = 0");
+        double max = n1;
+        double min = n4;
+        if (n2 > max) {
+            max = n2;
+        }
+        if (n3 > max) {
+            max = n3;
+        }
+        if (n4 > max) {
+             max = n4;
+        }
+        System.out.println("max = " + max);
+        if (n1 < min) {
+            min = n1;
+        }
+        if (n2 < min) {
+            min = n2;
+        }
+        if (n3 < min) {
+            min = n3;
+        }
+        System.out.println("min = " + min);
 
     }
 }
