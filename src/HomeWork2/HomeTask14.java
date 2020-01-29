313218fb1cd8f981349d0e81e30faabdd8b66b9a
SargisAmiryan <Sargisamiryan@gmail.com>
2020-01-29 17:49:43 +0400

    Homework2 14 arelem bay while chem ogtagorcel

--- src/HomeWork2/HomeTask14.java
+++ src/HomeWork2/HomeTask14.java
@@ -1,7 +1,19 @@
 package HomeWork2;
 
+import java.util.Scanner;
+
 public class HomeTask14 {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int firstNum = scanner.nextInt();
+    int secondNum = scanner.nextInt();
+    int sum=0;
+    for (int i=firstNum;i<=secondNum;i++){
+        if (i%2==0){
+            System.out.println( "even number" +i);
+        } else {sum=sum+i;}
 
-    }
+    }System.out.println("sum of all odd numbers = " +sum);
+
+}
 }
