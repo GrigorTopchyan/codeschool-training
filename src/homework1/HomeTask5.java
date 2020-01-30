30a7ba3287c6a2d0c2a873f76db6404dff1ada54
h-kocharyan <hkocharyan94@gmail.com>
2020-01-30 17:35:04 +0400

    task 5

--- src/homework1/HomeTask5.java
+++ src/homework1/HomeTask5.java
@@ -1,13 +1,30 @@
 package homework1;
 
+import java.util.Scanner;
+
 public class HomeTask5 {
-    public static void main(String[] args){
-          //Էս փորձի for ով անես կամայական n(մուտք արա կոնսոլից) ի համաար
-          System.out.println( "   *" );
-          System.out.println( "  ***" );
-          System.out.println( " *****" );
-          System.out.println( "*******" );
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int scan = scanner.nextInt();
+        for (int i = 1; i <= scan; i = i + 2) {
+            int spaceCount = (scan - i) / 2;
+            for (int j = 0; j < scan; j++) {
+                if (j < spaceCount || j >= (scan - spaceCount)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("$");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
 
 
+        }
     }
+
 }
+
+
+
+
+
