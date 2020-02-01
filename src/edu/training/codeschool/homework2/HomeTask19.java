f9e71dcae53f234938128eda0c04d5c2a93d5d51
h-kocharyan <hkocharyan94@gmail.com>
2020-02-01 15:00:25 +0400

    task 19

---
+++ src/edu/training/codeschool/homework2/HomeTask19.java
@@ -0,0 +1,17 @@
+package edu.training.codeschool.homework2;
+
+import java.util.Arrays;
+
+public class HomeTask19 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2};
+        int[] arr1 = {2, 3, 4};
+        int[] arr2 = {1, 2};
+        int[] arr3 = {1, 2};
+        System.out.println(Arrays.equals(arr, arr1));
+        System.out.println(Arrays.equals(arr, arr2));
+        System.out.println(Arrays.equals(arr, arr3));
+
+    }
+}
+
