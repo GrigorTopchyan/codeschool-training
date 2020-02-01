8c51ced02b4b7afd913d83e9bd45746471d17b0a
h-kocharyan <hkocharyan94@gmail.com>
2020-02-01 15:28:24 +0400

    task 8

---
+++ src/edu/training/codeschool/homework2/HomeTask8.java
@@ -0,0 +1,19 @@
+package edu.training.codeschool.homework2;
+
+public class HomeTask8 {
+    public static void main(String[] args) {
+//        Given array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49} and index = 4.
+//        Write a Java program to remove the element with the index from an array.
+//        Print the array before and after removal of the element
+        int[] arr = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println();
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (i != 4) {
+                System.out.print(arr[i] + ",");
+            }
+        }
+    }
+}
