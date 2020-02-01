aa7ab38060950a932d16eafd3a7234a9f6d9a792
h-kocharyan <hkocharyan94@gmail.com>
2020-02-01 15:09:20 +0400

    task 7

---
+++ src/edu/training/codeschool/homework2/HomeTask7.java
@@ -0,0 +1,26 @@
+package edu.training.codeschool.homework2;
+
+import java.util.Scanner;
+
+public class HomeTask7 {
+    public static void main(String[] args) {
+//        7. Given array = {1, 25, 8, 6, 4, 5, 56, 55, 8, 68, 45, 62, 66, 82, 63, 33, 3, 7, 71, 32}
+////        and n(Input n from console).
+////        Write a Java program that prints the index of the n if the array contains the specific value.
+////        Otherwise prints "The array does not contain the value"
+
+
+        Scanner scanner = new Scanner(System.in);
+        int scan = scanner.nextInt();
+        int[] array = {1, 25, 8, 6, 4, 5, 56, 55, 8, 68, 45, 62, 66, 82, 63, 33, 3, 7, 71, 32};
+        for (int i = 0; i < array.length - 1; i++) {
+            if (scan == array[i]) {
+                System.out.println(i);
+            }
+            System.out.println("The array does not contain the value");
+            break;
+        }
+
+    }
+
+}
