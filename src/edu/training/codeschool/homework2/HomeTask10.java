229ca61030c0663719a2ede924a9cc64c3442b1c
h-kocharyan <hkocharyan94@gmail.com>
2020-02-02 15:27:16 +0400

    task 10

---
+++ src/edu/training/codeschool/homework2/HomeTask10.java
@@ -0,0 +1,25 @@
+package edu.training.codeschool.homework2;
+
+public class HomeTask10 {
+    public static void main(String[] args) {
+//        10. Given array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49}
+//        Write a Java program to find the maximum and minimum value of an array.
+        int[] arr = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int max = arr[5];
+        int min = arr[3];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                System.out.println("The max is " + max);
+                break;
+            }
+        }
+        for (int j = 0; j <= arr.length - 1; j++) {
+            if (arr[j] < min) {
+            }
+            min = arr[j];
+            System.out.println("The min is " + min);
+            break;
+        }
+    }
+}
