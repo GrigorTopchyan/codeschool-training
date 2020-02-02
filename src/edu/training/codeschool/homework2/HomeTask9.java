021c6f49bf10b3dd617e0dd5893ee43fa0f720af
h-kocharyan <hkocharyan94@gmail.com>
2020-02-02 15:36:29 +0400

    task 10 uxxvac

---
+++ src/edu/training/codeschool/homework2/HomeTask9.java
@@ -0,0 +1,14 @@
+package edu.training.codeschool.homework2;
+
+public class HomeTask9 {
+    public static void main(String[] args) {
+//        9. Given array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49}.
+//                Write a Java program to copy the array to another array.
+        int[] arr = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int[] arr1;
+
+
+
+
+    }
+}
