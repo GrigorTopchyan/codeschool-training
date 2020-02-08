ca9f8ac5c33a0bca1a7e63134f46da4c8c6426be
h-kocharyan <hkocharyan94@gmail.com>
2020-02-08 13:29:31 +0400

    TestPersonSubClasses

--- src/edu/training/codeschool/HomeWork/TestPersonSubClasses.java
+++ src/edu/training/codeschool/HomeWork/TestPersonSubClasses.java
@@ -1,10 +1,14 @@
 package edu.training.codeschool.HomeWork;
 
+import edu.training.codeschool.classes_objects.homework4.Circle;
+
 public class TestPersonSubClasses {
     public static void main(String[] args) {
         Studen1 studen1=new Studen1("Hayk", "Yerevan", "Petakan",25000);
         Employ employ=new Employ("Gegham","Vanadzor","Grand Holding",4500000);
         System.out.println(employ.toString());
         System.out.println(studen1.toString());
+
+
     }
 }
