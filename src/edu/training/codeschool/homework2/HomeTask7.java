56ca60ded5263eb1fd3867e74a5af611a9ec69d5
h-kocharyan <hkocharyan94@gmail.com>
2020-02-08 18:46:42 +0400

    task 7 erkrord tnayinneric

--- src/edu/training/codeschool/homework2/HomeTask7.java
+++ src/edu/training/codeschool/homework2/HomeTask7.java
@@ -8,22 +8,24 @@ public class HomeTask7 {
 ////        and n(Input n from console).
 ////        Write a Java program that prints the index of the n if the array contains the specific value.
 ////        Otherwise prints "The array does not contain the value"
-
-
         Scanner scanner = new Scanner(System.in);
         int scan = scanner.nextInt();
+        int a = 0;
         int[] array = {1, 25, 8, 6, 4, 5, 56, 55, 8, 68, 45, 62, 66, 82, 63, 33, 3, 7, 71, 32};
-        //i < array.length ոչ թէ i < array.length - 1
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (scan == array[i]) {
+                a++;
                 System.out.println(i);
             }
-            //էսի պիտի for ից դուրս լինի
+        }
+        if (a == 0) {
             System.out.println("The array does not contain the value");
-            //Էս break ի տեղն էլա սխալ դու պիտի break անես երբ գտել ես
-            break;
         }
 
-    }
 
+    }
 }
+
+
+
+
