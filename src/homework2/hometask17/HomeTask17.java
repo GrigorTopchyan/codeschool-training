7007cf1bfd55a94e3f304816199a27e9d185db57
ArsenYeghikyan <arsegikyan@gmail.com>
2020-02-10 18:31:16 +0400

    homework2

--- src/homework2/hometask17/HomeTask17.java
+++ src/homework2/hometask17/HomeTask17.java
@@ -6,26 +6,19 @@ public class HomeTask17 {
      * ex. {4,5,7,8,3} -> 7is second largest element, second smallest 4
      */
     public static void main(String[] args) {
-        int[] array = {4, 5, 117, 8, 3, 98, 100};
-        int firstMax = array[0];
-        int secondMax = array[0];
+        int[] array = {449, 5, 200, 8, 3, 100};
+        int firstMax = 0;
+        int secondMax = 0;
 //
-        //Նույն մինիմում մաքսիմումի նմանա բայց երկու հատ min երկու հատ max պիտի պահես
-        //փորձի գրես
-
-        for (int i = 0; i <= array.length - 1; i++) {
-            if (array[i]>firstMax){
-               firstMax=array[i];
-            }
-
-        }
 
 
-        System.out.println(firstMax);
-        for (int i = 0; i <=array.length-1 ; i++) {
-            if (firstMax<array[i]){
-                secondMax=firstMax;
-                firstMax=array[i];
+        for (int i = 0; i < array.length; i++) {
+            if ( array[i]>firstMax) {
+                secondMax = firstMax;
+                firstMax = array[i];
+            }else if ( array[i]>secondMax) {
+
+                secondMax = array[i];
             }
         }
         System.out.println(secondMax);
