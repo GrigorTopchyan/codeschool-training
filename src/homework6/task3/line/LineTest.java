b930bf1ffd5ec6e23ef1b8effe3176807fe31aa8
ArsenYeghikyan <arsegikyan@gmail.com>
2020-02-15 10:52:44 -0800

    homework5 edited

--- src/homework6/task3/line/LineTest.java
+++ src/homework6/task3/line/LineTest.java
@@ -2,7 +2,7 @@ package homework6.task3.line;
 
 public class LineTest {
     public static void main(String[] args) {
-        Line line = new Line(3,2,2,2,2,2);
-        System.out.println(line.isPointOnLine());
+//        Line line = new Line(3,2,2,2,2,2);
+//        System.out.println(line.isPointOnLine());
     }
 }
